55a4e7f80944877529da23f138a75a3a8a44e84d
giuseppe aurilio <peppinus79@hotmail.com>
2019-09-06 00:18:37 +0200

    stile e icone sul menu

--- site/menu.php
+++ site/menu.php
@@ -163,9 +163,9 @@ $anno=$row["valore"];
 
 <div class="stellarnav">
 		  <ul class="navbar">
-			 <li><a href="/homepage/home.php">Home</a></li>
-			 <li><a href="/display_classifiche.php">Classifiche</a></li>
-			 <li><a href="#">Calendari</a>
+			 <li><a href="/homepage/home.php"> <i class="fas fa-home"></i> Home</a></li>
+			 <li><a href="/display_classifiche.php"><i class="fas fa-chart-line"></i> Classifiche</a></li>
+			 <li><a href="#"><i class="fas fa-calendar-alt"></i> Calendari</a>
 				<ul>
 				   <li><a href="/display_calendario.php?&id_girone=1">Apertura</a></li>
 					 <li><a href="/display_calendario.php?&id_girone=2">Chiusura</a></li>
@@ -175,24 +175,24 @@ $anno=$row["valore"];
 				   <li><a href="/display_calendario_finali.php">Finali</a></li>
 				</ul>         
 			 </li>
-			 <li><a href="#">Rose</a>
+			 <li><a href="#"><i class="fas fa-users"></i> Rose</a>
 				<ul>
 				   <li><a href="/display_rose.php" >Susy League</a></li>
 				   <li><a href="/display_giocatori.php">Serie A</a></li>
 				   <li><a href="/display_asta.php">Asta</a></li>
 				</ul>         
 			 </li>
-			 <li><a href="/invio_formazione.php">Invio formazione</a></li>
-			 <li><a href="#">Social</a>
+			 <li><a href="/invio_formazione.php"><i class="fas fa-futbol"></i> Invio formazione</a></li>
+			 <li><a href="#"><i class="far fa-thumbs-up"></i> Social</a>
 				<ul>
 				   <li><a href="/display_sondaggi.php">Sondaggi</a></li>
 				   <li><a href="/display_mercato.php">Mercato</a></li>
 
 				</ul>         
 			 </li>
-			 <li><a href="/cambia_password.php" >Password</a></li>
-			 <li><a href="/homepage/regolamento.pdf" >Regolamento</a></li>
-			 <li><a href="/presidente/amministrazione.php" >Amministrazione</a></li>
+			 <li><a href="/cambia_password.php" ><i class="fas fa-lock"></i> Password</a></li>
+			 <li><a href="/homepage/regolamento.pdf" ><i class="fas fa-pencil-alt"></i> Regolamento</a></li>
+			 <li><a href="/presidente/amministrazione.php" ><i class="fas fa-tools"></i> Amministrazione</a></li>
 		  </ul>
 		 </div>
 
