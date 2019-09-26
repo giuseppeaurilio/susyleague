b5fa0dd5ea6b37893f14f863540c9f53e75b1dc4
giuseppe aurilio <peppinus79@hotmail.com>
2019-09-26 17:48:15 +0200

    nuova revision

--- site/footer.php
+++ site/footer.php
@@ -3,7 +3,7 @@
 <span style="float:left; display: block">
     powered by: susyleague plc.
     </span>
-    <span style="float:right; display: block"> web site V1.2</span>
+    <span style="float:right; display: block"> web site V1.3</span>
 </footer>
 
 
