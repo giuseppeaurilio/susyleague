749b941da007c6e944c3b245d727528583ae8dbd
giuseppe aurilio <peppinus79@hotmail.com>
2020-09-09 16:57:32 +0200

    coppa delle coppe a 12 partecipanti

--- site/presidente/coppacoppe_girone.php
+++ site/presidente/coppacoppe_girone.php
@@ -3,49 +3,55 @@ include("menu.php");
 
 ?>
 <script>
-    SalvaGirone = function(){
-        // alert($(this).attr('id'));
-        var idsquadre = [];
-        for (x = 1; x <= 10; x++) {
-            var d = $("#sq_fc"  + x + " option:selected").val();
-            if(d == null || d == 0 ){
-                alert('selezionare tutte le squadre del girone')
-                return false;
-            }
-            idsquadre.push(d);
-        }
-        // alert (dati);
-        $.ajax({
-                type:'POST',
-                url:'coppacoppe_c_salvagiorone.php',
-                data: {"idsquadre": JSON.stringify(idsquadre)},
-                success:function(data){
-                    // debugger;
-                    var resp=$.parseJSON(data)
-                    if(resp.result == "true"){
-                        alert(resp.message);
-                    }
-                    else{
-                        alert(resp.error.msg);
-                    }
+    // SalvaGirone = function(){
+    //     // alert($(this).attr('id'));
+    //     var idsquadre = [];
+        
+    //     for (x = 1; x <= 10; x++) {
+    //         var d = $("#sq_fc"  + x + " option:selected").val();
+    //         if(d == null || d == 0 ){
+    //             alert('selezionare tutte le squadre del girone')
+    //             return false;
+    //         }
+    //         idsquadre.push(d);
+    //     }
+    //     alert (dati);
+        
+    //     $.ajax({
+    //             type:'POST',
+    //             url:'coppacoppe_c_salvagiorone.php',
+    //             data: {"idsquadre": JSON.stringify(idsquadre)},
+    //             success:function(data){
+    //                 // debugger;
+    //                 var resp=$.parseJSON(data)
+    //                 if(resp.result == "true"){
+    //                     alert(resp.message);
+    //                 }
+    //                 else{
+    //                     alert(resp.error.msg);
+    //                 }
                     
-                    //$('#city').html('<option value="">Select state first</option>'); 
-                }
-                // ,error: function (xhr, ajaxOptions, thrownError) {
-                //     alert(xhr.responseText);
-                // }
-        }); 
-    }
+    //                 //$('#city').html('<option value="">Select state first</option>'); 
+    //             }
+    //             // ,error: function (xhr, ajaxOptions, thrownError) {
+    //             //     alert(xhr.responseText);
+    //             // }
+    //     }); 
+    // }
     GeneraCalendario = function(){
         var idsquadre = [];
-        for (x = 1; x <= 10; x++) {
-            var d = $("#sq_fc"  + x + " option:selected").val();
-            if(d == null || d == 0 ){
-                alert('selezionare tutte le squadre del girone')
-                return false;
-            }
-            idsquadre.push(d);
-        }
+        // for (x = 1; x <= 10; x++) {
+        //     var d = $("#sq_fc"  + x + " option:selected").val();
+        //     if(d == null || d == 0 ){
+        //         alert('selezionare tutte le squadre del girone')
+        //         return false;
+        //     }
+        //     idsquadre.push(d);
+        // }
+        $('.coppacoppasquadra').each(function( index ) {
+            if($(this).is(":checked"))
+                idsquadre.push($(this).val());
+            });
         //var dati = JSON.stringify({"girone": girone, "idsquadre": idsquadre});
         // alert(JSON.stringify(idsquadre));
         $.ajax({
@@ -69,55 +75,55 @@ include("menu.php");
                 // }
             });
     }
-    showhideOption = function(){
-        var value =$(this).val();
-        var hf = $("#hf" +  $(this).attr("id").slice(-1).toLowerCase());
-        // alert(value);
-        if(value != null && value != ""){
-            $("select option[value=" + $(this).val() + "]").hide();
-            hf.val($(this).val());
-        }    
-        else{
-            $("select option[value=" + hf.val() + "]").show();
-            hf.val("");
-        }
-    };
-    CaricaGirone = function(){
-        $.ajax({
-                type:'POST',
-                url:'coppacoppe_c_getgirone.php',
-                data: {"girone": "unico"},
-                dataType:"json", 
-                success:function(data){
-                    // debugger;
-                    // var resp=$.parseJSON(data)
-                    if(data.result == "true"){
-                        // alert(data.id_numbers);
-                        for (i = 0; i < data.id_numbers.length; i++) { 
-                            $("#sq_fc" + (i+1)).val(data.id_numbers[i]["id_squadra"]).trigger('change');
-                            $("#cb"+ (i+1)).prop('checked', data.id_numbers[i]["squadra_materasso"] == "1");
-                        }
-                    }
-                    else{
-                        alert(data.error.msg);
-                    }
-                    //$('#city').html('<option value="">Select state first</option>'); 
-                }
-                // ,error: function (xhr, ajaxOptions, thrownError) {
-                //     alert(xhr.responseText);
-                // }
-            });
-    }
+    // showhideOption = function(){
+    //     var value =$(this).val();
+    //     var hf = $("#hf" +  $(this).attr("id").slice(-1).toLowerCase());
+    //     // alert(value);
+    //     if(value != null && value != ""){
+    //         $("select option[value=" + $(this).val() + "]").hide();
+    //         hf.val($(this).val());
+    //     }    
+    //     else{
+    //         $("select option[value=" + hf.val() + "]").show();
+    //         hf.val("");
+    //     }
+    // };
+    // CaricaGirone = function(){
+    //     $.ajax({
+    //             type:'POST',
+    //             url:'coppacoppe_c_getgirone.php',
+    //             data: {"girone": "unico"},
+    //             dataType:"json", 
+    //             success:function(data){
+    //                 // debugger;
+    //                 // var resp=$.parseJSON(data)
+    //                 if(data.result == "true"){
+    //                     // alert(data.id_numbers);
+    //                     for (i = 0; i < data.id_numbers.length; i++) { 
+    //                         $("#sq_fc" + (i+1)).val(data.id_numbers[i]["id_squadra"]).trigger('change');
+    //                         $("#cb"+ (i+1)).prop('checked', data.id_numbers[i]["squadra_materasso"] == "1");
+    //                     }
+    //                 }
+    //                 else{
+    //                     alert(data.error.msg);
+    //                 }
+    //                 //$('#city').html('<option value="">Select state first</option>'); 
+    //             }
+    //             // ,error: function (xhr, ajaxOptions, thrownError) {
+    //             //     alert(xhr.responseText);
+    //             // }
+    //         });
+    // }
     $(document).ready(function(){
 
         
-        $("#salvagirone").off("click").bind("click", SalvaGirone);
+        // $("#salvagirone").off("click").bind("click", SalvaGirone);
 
         $("#generacalendariogirone").off("click").bind("click", GeneraCalendario);
 
-        $( "select").change(showhideOption);
+        // $( "select").change(showhideOption);
 
-        CaricaGirone();
+        // CaricaGirone();
     })
 </script>
 
@@ -125,11 +131,11 @@ include("menu.php");
 <h2>Girone Coppa delle Coppe </h2>
 <div >
 <?php
-$index =1;
-while ($index <= 10){
-    echo '<input type="hidden" id="hf'.$index.'"/>';
-    echo '<select id="sq_fc' .$index.'" name="squadra_fantacalcio">';
-    echo '<option value="">--Seleziona squadra fantacalcio--</option>';
+// $index =1;
+// while ($index <= 10){
+//     echo '<input type="hidden" id="hf'.$index.'"/>';
+//     echo '<select id="sq_fc' .$index.'" name="squadra_fantacalcio">';
+//     echo '<option value="">--Seleziona squadra fantacalcio--</option>';
         
 
     $query="SELECT * FROM sq_fantacalcio order by squadra";
@@ -142,17 +148,17 @@ while ($index <= 10){
         // $id=mysql_result($result,$i,"id");
         $id=$row["id"];
         $squadra=$row["squadra"];
-        echo '<option value=' . $id . '>'. $squadra . '</option>';
-    ++$i;
+        echo '<input id="cbSquadra'.$id.'" class="coppacoppasquadra" type="checkbox" checked  value="'.$id.'">'. $squadra . '</input><br>';
+    // ++$i;
     }
 
-    echo '</select>';
-    echo '<br/>';
-    $index++;
-}
+//     echo '</select>';
+//     echo '<br/>';
+//     $index++;
+// }
 ?>
 
-<input type="button" id="salvagirone" value="Salva"/>
+<!-- <input type="button" id="salvagirone" value="Salva"/> -->
 <input type="button" id="generacalendariogirone" value="Genera Calendario"/>
 </div>
 
