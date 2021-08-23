c5627ebd46e7c346d9c54bc9919bec7b2a0fd2fc
giuseppe aurilio <gaurilio@pmeasuring.com>
2021-08-23 14:00:29 +0200

    permetto la ricerca di un giocatore, dei giocatori per ruolo, dei giocatori per squadra.

--- site/presidente/amministra_voti.php
+++ site/presidente/amministra_voti.php
@@ -69,6 +69,9 @@ getVoti = function()
                     // control.parent().find(".resultcontainer").html(rendered);
                     $("#resultcontainer").html(rendered)
                     $(".updateVoto").off("click").bind("click", updateVoto);
+                    $("#tblGiocatori #txtNome").unbind().bind("keyup", filtraGiocatoriPerNome);
+                    $("#tblGiocatori #ruolo").unbind().bind("change", filtraGiocatoriPerRuolo);
+                    $("#tblGiocatori #squadra").unbind().bind("change", filtraGiocatoriPerSquadraBreve);
                     // debugger;
                     if(resp.voti.length == 0){
                         $("#formUploadVoti").show();
@@ -86,10 +89,61 @@ getVoti = function()
             }
     }); 
 }
+
+filtraGiocatoriPerNome = function()
+{
+    var filterString = $("#tblGiocatori #txtNome").val().toUpperCase();
+    // console.log(filterString);
+    $("#tblGiocatori > tbody tr").show()
+    if(filterString != "" && filterString.length >= 2)
+    {
+        $("#tblGiocatori > tbody tr").hide()
+        $($("#tblGiocatori > tbody tr")[0]).show()
+        $("#tblGiocatori .nome").filter(function() {
+            return $(this).html().includes(filterString);
+        }).closest('tr').show()
+    }
+    
+}
+
+filtraGiocatoriPerRuolo = function()
+{
+    // debugger;
+    var filterString = $("#tblGiocatori #ruolo").val().toUpperCase();
+    // console.log(filterString);
+    $("#tblGiocatori > tbody tr").show()
+    if(filterString != "" )
+    {
+        $("#tblGiocatori > tbody tr").hide()
+        $($("#tblGiocatori > tbody tr")[0]).show()
+        $("#tblGiocatori .ruolo").filter(function() {
+            return $(this).html().includes(filterString);
+        }).closest('tr').show()
+    }
+    
+}
+
+filtraGiocatoriPerSquadraBreve = function()
+{
+    // debugger;
+    var filterString = $("#tblGiocatori #squadra").val().toUpperCase();
+    // console.log(filterString);
+    $("#tblGiocatori > tbody tr").show()
+    if(filterString != "" )
+    {
+        $("#tblGiocatori > tbody tr").hide()
+        $($("#tblGiocatori > tbody tr")[0]).show()
+        $("#tblGiocatori .sq_breve").filter(function() {
+            return $(this).html().includes(filterString);
+        }).closest('tr').show()
+    }
+    
+}
+
 var idgiornatasa = "<?php echo $giornata_serie_a_id=isset($_GET['giornata_serie_a_id']) ? $_GET['giornata_serie_a_id']: '' ; ?>";
 $(document).ready(function(){
     $("#ddlGiornataSerieA").off("change").bind("change", getVoti);
-    $("#divCancellaVoti").off("click").bind("click", cancellaVoti);
+    $("#divCancellaVoti > a").off("click").bind("click", cancellaVoti);
    
     $("#formUploadVoti").hide();
     $("#divCancellaVoti").hide();
@@ -104,17 +158,52 @@ $(document).ready(function(){
     // $("#btnUploadFile").off("submit").bind("submit", uploadFile);
 })
 </script>
+<?php
+    //load squadre fantacalcio
+    $query="SELECT * FROM squadre_serie_a order by squadra";
 
+    $result=$conn->query($query);
+    $squadre = array();
+    while($row = $result->fetch_assoc()){
+        // $id=mysql_result($result,$i,"id");
+        // $id=$row["id"];
+        // $squadra=$row["squadra"];
+        array_push($squadre, array(
+            "id"=>$row["id"],
+            "squadra_breve"=>$row["squadra_breve"]
+            )
+        );
+    }
+    //fine load squadre fantacalcio
+    ?>
 <script id="tmplVoti" type="x-tmpl-mustache">
     
-<table border="0" cellspacing="2" cellpadding="2">
+<table border="0" cellspacing="2" cellpadding="2" id="tblGiocatori">
     <tbody>
         <tr> 
             <th style="display:none;">Id</th>
             <th>Id G.</th>
-            <th>Nome</th>
-            <th>R</th>
-            <th>Squadra</th>
+            <th ><input style="width:140px;" type="text" id="txtNome" placeholder="Nome"></th>
+            <th>
+                <select name="Ruolo" id="ruolo">
+                    <option value="">-R</option>	
+                    <option value="P">P</option>
+                    <option value="D">D</option>
+                    <option value="C">C</option>
+                    <option value="A">A</option>
+                </select>
+            </th>
+            <th>
+                <?php
+                    echo '<select id="squadra" name="squadra">';
+                    echo '<option value="">-sq-</option>';
+                    foreach($squadre as $squadra)
+                    {
+                        echo '<option value=' . $squadra["squadra_breve"] . '>'. $squadra["squadra_breve"] . '</option>';
+                    }
+                    echo '</select>';
+                ?>
+            </th>
             <th>FV</th>
             <th>V</th>
             <th>&nbsp;</th>
@@ -123,9 +212,9 @@ $(document).ready(function(){
             <tr > 
                 <td style="display:none;">{{ id }}</td>
                 <td>{{ g_id }}</td>
-                <td>{{ nome }}</td>
-                <td>{{ ruolo }}</td>
-                <td>{{ squadra }}</td>
+                <td class="nome" >{{ nome }}</td>
+                <td class="ruolo">{{ ruolo }}</td>
+                <td class="sq_breve">{{ squadra }}</td>
                 <td><input type="number" id="txtFV{{ id }}" min="-10" max="30" step="0.5" value="{{ voto }}" style="width: 50px;"></td>
                 <td><input type="number" id="txtVoto{{ id }}" min="-10" max="30" step="0.5" value="{{ voto_md }}" style="width: 50px;"></td>
                 <td><input type="button" class="updateVoto" data-id="{{ id }}" value="aggiorna"></td>
