52def6e632c7515d4f4e8aeb2fa3bc862d853de3
giuseppe aurilio <gaurilio@pmeasuring.com>
2022-03-02 22:11:56 +0100

    formattazione e debugger

--- site/presidente/coppaitalia_tabellone.php
+++ site/presidente/coppaitalia_tabellone.php
@@ -11,10 +11,10 @@ salvaSquadreCITabellone = function(){
     var giornatarit = parseInt(giornataand) +1;
     var id1 = $("#sq_a_" +  giornataand + " option:selected").val();
     var id2 = $("#sq_b_" +  giornataand + " option:selected").val();
-    debugger;
+    // debugger;
     salvaSquadrePartita(id1, id2, giornataand);
     // if(giornatarit != 77)
-        salvaSquadrePartita(id2, id1, giornatarit);
+    salvaSquadrePartita(id2, id1, giornatarit);
 };
 salvaGiornataCITabellone = function(){
     // console.log($(this).attr("data-idgiornata"));
