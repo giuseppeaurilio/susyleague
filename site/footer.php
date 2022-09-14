1a0d34b489962651291c533e53fb87224cfb288f
giuseppe aurilio <gaurilio@pmeasuring.com>
2022-09-14 13:02:40 +0200

    Aggiornamento versione

--- site/footer.php
+++ site/footer.php
@@ -3,7 +3,7 @@
 <span style="float:left; display: block">
     powered by: susyleague plc.
     </span>
-    <span style="float:right; display: block">V2.5.1</span>
+    <span style="float:right; display: block">V2.6.0</span>
    
 </footer>
 <button id="btnToTop" title="Torna su" class="back-to-top " style="display: none;">
