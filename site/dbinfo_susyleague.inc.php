48686aa408f97db7b4ec954e4af95cfb62ea53e8
giuseppe aurilio <gaurilio@pmeasuring.com>
2024-08-30 13:23:26 +0200

    per rilascio

--- site/dbinfo_susyleague.inc.php
+++ site/dbinfo_susyleague.inc.php
@@ -1,18 +1,16 @@
 <?php
 function getConnection(){
-    //localhost
-    $username="id258940_susy79";
-    $password="andspe79";
-    // $database="id258940_susy_league_2023-24";
-    // $database="id258940_susy_league_2022-23_asta";
-     $database="id258940_susy_league_2022-23";
-    $localhost = "localhost";
+    // //localhost
+    // $username="id258940_susy79";
+    // $password="andspe79";
+    // $database="id258940_susy_league_2024-25";
+    // $localhost = "localhost";
 
-    // //tophost
-    // $username="susyleag16215";
-    // $password="*q3G99i&w24J";
-    // $database="susyleag16215";
-    // $localhost = "sql.susyleague.it";
+    //tophost
+    $username="susyleag16215";
+    $password="*q3G99i&w24J";
+    $database="susyleag16215";
+    $localhost = "sql.susyleague.it";
 
 
     // Create connection
