2d4a8d60984703645bc42d1616ffe9c1d5975720
giuseppe aurilio <gaurilio@pmeasuring.com>
2025-08-18 11:46:44 +0200

    Aggiornato albo d'oro 24 25

--- site/display_AlboDOro.php
+++ site/display_AlboDOro.php
@@ -26,7 +26,148 @@ $(document).ready(function(){
 	<div id="tabs-1">
 		<h3>Campionato Susy league</h3>
 		<div  class="content">
-		<div class="anno">2023/2024</div>
+			<div class="anno">2024/2025</div>
+			<div class="campionato">
+				<div class="competizione">
+					<div class="descrizione"> Finale Campionato </div>
+					<div class="vincitori">
+						<div class="primo">
+							<div><i class="fas fa-shield-alt"></i> </div>
+							<div class="titolo">1°</div>
+							<div class="squadra">AS Frieskies</div>
+							<div class="allenatore">(Antonio e Michela)</div>
+						</div>
+						<div class="secondo">
+							<div><i class="fas fa-medal"></i></div>
+							<div class="titolo">2°</div>
+							<div class="squadra">Titolari lo cost</div>
+							<div class="allenatore">(Filippo Pagliarella)</div>
+						</div>
+					</div>
+				</div>	
+				<div class="competizione">
+					<div class="descrizione">Aggregate - Campionato</div>
+					<div class="vincitori">
+						<div class="primo">
+							<div><i class="fas fa-award"></i></div>
+							<div class="titolo">1°</div>
+							<div class="squadra">Bar 58</div>
+							<div class="allenatore">(Daniele Rotondo)</div>
+						</div>
+						<div class="secondo">
+							<div><i class="fas fa-award"></i></div>
+							<div class="titolo">2° </div>
+							<div class="squadra">Titolari lo cost</div>
+							<div class="allenatore">(Filippo Pagliarella)</div>
+						</div>
+						<div class="terzo">
+							<div><i class="fas fa-award"></i></div>
+							<div class="titolo">3°</div>
+							<div class="squadra">MILANdruccolo</div>
+							<div class="allenatore">(Vezio Malandruccolo)</div>
+						</div>
+					</div>
+				</div>
+				<div class="competizione">
+					<div class="descrizione">Aggregate - Cannonieri</div>
+					<div class="vincitori">
+						<div class="primo">
+							<div><i class="far fa-futbol"></i></div>
+							<div class="titolo">1° </div>
+							<div class="squadra">Team chiappetto Iori</div>
+							<div class="allenatore">(Marco Arianna)</div>
+						</div>
+						<div class="secondo">
+							<div><i class="fas fa-award"></i></div>
+							<div class="titolo">2°</div>
+							<div class="squadra">AS Frieskies</div>
+							<div class="allenatore">(Antonio e Michela)</div>
+						</div>
+					</div>
+				</div>
+				<div class="competizione">
+					<div class="descrizione">Apertura - Campionato</div>
+					<div class="vincitori">
+						<div class="primo">
+							<div><i class="fas fa-award"></i></div>
+							<div class="titolo">1°</div>
+							<div class="squadra">Titolari lo cost</div>
+							<div class="allenatore">(Filippo Pagliarella)</div>
+						</div>
+						<div class="secondo">
+							<div><i class="fas fa-award"></i></div>
+							<div class="titolo">2° </div>
+							<div class="squadra">Bar 58</div>
+							<div class="allenatore">(Daniele Rotondo)</div>
+						</div>
+						<div class="terzo">
+							<div><i class="fas fa-award"></i></div>
+							<div class="titolo">3°</div>
+							<div class="squadra">MILANdruccolo</div>
+							<div class="allenatore">(Vezio Malandruccolo)</div>
+						</div>
+					</div>
+				</div>
+				<div class="competizione">
+					<div class="descrizione">Apertura - Cannonieri</div>
+					<div class="vincitori">
+						<div class="primo">
+							<div><i class="far fa-futbol"></i></div>
+							<div class="titolo">1° </div>
+							<div class="squadra">Team chiappetto Iori</div>
+							<div class="allenatore">(Marco Arianna)</div>
+						</div>
+						<div class="secondo">
+							<div><i class="fas fa-award"></i></div>
+							<div class="titolo">2°</div>
+							<div class="squadra">Riommelu</div>
+							<div class="allenatore">(Carletto Sciandrone)</div>
+						</div>
+					</div>
+				</div>
+				<div class="competizione">
+					<div class="descrizione">Chiusura - Campionato</div>
+					<div class="vincitori">
+						
+						<div class="primo">
+							<div><i class="fas fa-award"></i></div>
+							<div class="titolo">1°</div>
+							<div class="squadra">AS Frieskies</div>
+							<div class="allenatore">(Antonio e Michela)</div>
+						</div>
+						<div class="secondo">
+							<div><i class="fas fa-award"></i></div>
+							<div class="titolo">2°</div>
+							<div class="squadra">Sudomagodo</div>
+							<div class="allenatore">(Giorgio Gasbarrini)</div>
+						</div>
+						<div class="terzo">
+							<div><i class="fas fa-award"></i></div>
+							<div class="titolo">3°</div>
+							<div class="squadra">Bar 58</div>
+							<div class="allenatore">(Daniele Rotondo)</div>
+						</div>
+					</div>
+				</div>
+				<div class="competizione">
+					<div class="descrizione">Chiusura - Cannonieri</div>
+					<div class="vincitori">
+						<div class="primo">
+							<div><i class="far fa-futbol"></i></div>
+							<div class="titolo">1° </div>
+							<div class="squadra">AS Frieskies</div>
+							<div class="allenatore">(Antonio e Michela)</div>
+						</div>
+						<div class="secondo">
+							<div><i class="fas fa-award"></i></div>
+							<div class="titolo">2°</div>
+							<div class="squadra">Team chiappetto Iori</div>
+							<div class="allenatore">(Marco Arianna)</div>
+						</div>
+					</div>
+				</div>		
+			</div>
+			<div class="anno">2023/2024</div>
 			<div class="campionato">
 				<div class="competizione">
 					<div class="descrizione"> Finale Campionato </div>
@@ -1565,6 +1706,23 @@ $(document).ready(function(){
 	<div id="tabs-2">
 		<h3>Coppa delle Coppe <span class="note"> (*prima edizione 2012)</span></h3>
 		<div class="content">
+		<div class="anno">2025</div>
+			<div class="coppacoppe">
+				<div class="vincitori competizione2">
+					<div class="primo">
+						<div><i class="fas fa-trophy"></i> </div>
+						<div class="titolo">1°</div>
+						<div class="squadra">Riommelu</div>
+						<div class="allenatore">(Carletto Sciandrone)</div>
+					</div>
+					<div class="secondo">
+						<div><i class="fas fa-medal"></i> </div>
+						<div class="titolo">2°</div>
+						<div class="squadra">Team chiappetto Iori</div>
+						<div class="allenatore">(Marco Arianna)</div>
+					</div>
+				</div>
+			</div>
 		<div class="anno">2024</div>
 			<div class="coppacoppe">
 				<div class="vincitori competizione2">
@@ -1793,6 +1951,23 @@ $(document).ready(function(){
 	<div id="tabs-3">
 		<h3>Coppa Italia<span class="note"> (*prima edizione 2019/20)</span></h3>
 		<div class="content">
+		<div class="anno">2024/25</div>
+			<div class="coppaitalia">
+				<div class="vincitori competizione2">
+					<div class="primo">
+						<div><i class="fas fa-trophy"></i> </div>
+						<div class="titolo">1°</div>
+						<div class="squadra">Team chiappetto Iori</div>
+						<div class="allenatore">(Marco Arianna)</div>
+					</div>
+					<div class="secondo">
+					<div><i class="fas fa-medal"></i> </div>
+						<div class="titolo">2°</div>
+						<div class="squadra">Pianeta Verde NA</div>
+						<div class="allenatore">(Andrea Rotondo)</div>
+					</div>
+				</div>
+			</div>	
 		<div class="anno">2023/24</div>
 			<div class="coppaitalia">
 				<div class="vincitori competizione2">
@@ -1884,6 +2059,23 @@ $(document).ready(function(){
 	<div id="tabs-4">
 		<h3>Supercoppa<span class="note"> (*prima edizione 2020/21)</span></h3>
 		<div class="content">
+		<div class="anno">2024/25</div>
+			<div class="supercoppa">
+				<div class="vincitori competizione2">
+					<div class="primo">
+						<div><i class="fas fa-trophy"></i> </div>
+						<div class="titolo">1°</div>
+						<div class="squadra">Cugino Bruno </div>
+						<div class="allenatore">(Mario De Persis)</div>
+					</div>
+					<div class="secondo">
+					<div><i class="fas fa-medal"></i> </div>
+						<div class="titolo">2°</div>
+						<div class="squadra">Pianeta Verde NA</div>
+						<div class="allenatore">(Andrea Rotondo)</div>
+					</div>
+				</div>
+			</div>	
 		<div class="anno">2023/24</div>
 			<div class="supercoppa">
 				<div class="vincitori competizione2">
@@ -1958,7 +2150,19 @@ $(document).ready(function(){
 	<div id="tabs-5">
 		<h3>Fustometro<span class="note"> (*prima edizione 2019/20)</span></h3>
 		<div class="content">
-		<div class="anno">2023/24</div>
+			<div class="anno">2024/25</div>
+			<div class="Fustometro">
+				<div class="vincitori competizione2">
+					<span> <i class="fas fa-beer"></i>Team Chiappette Iori: Ma perch sei avvocato veramente</span><br>
+					<span> <i class="fas fa-beer"></i>BAR 58: Il presidente risponde quando ci pare e piace</span><br>
+					<span> <i class="fas fa-beer"></i>Team chiappetto Iori: Spamming gufaccio e insulti indiscriminati</span><br>
+					<span> <i class="fas fa-beer"></i>Pianeta Verde NA: Mette le mani avanti pe ne remane arete</span><br>
+					<span> <i class="fas fa-beer"></i>MILANndruccolo : Uso ripetuto della frase non chiaro</span><br>
+					<span> <i class="fas fa-beer"></i>I NANI: Problemi di udito e galanteria sugli autorilanci</span><br>
+					<span> <i class="fas fa-beer"></i>BAR 58: Vietato indossare la maglia Presidente del Presidente e soprattutto farsela cadere a pennello</span><br>
+				</div>
+			</div>		
+			<div class="anno">2023/24</div>
 			<div class="Fustometro">
 				<div class="vincitori competizione2">
 					<span> <i class="fas fa-beer"></i> Presidente : Ergo un fusto</span><br>
